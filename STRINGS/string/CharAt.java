b88a41569ed5d7ed8243c8809d402c4365ad01cd
narasimharedlapalli <124525597+narasimharedlapalli@users.noreply.github.com>
2023-02-06 15:51:08 +0530

    Charat

---
+++ STRINGS/string/CharAt.java
@@ -0,0 +1,8 @@
+package string;
+
+public class CharAt{
+    public static void main(String args[]){
+        String name="javatpoint";
+        char ch=name.charAt(4);/
+        System.out.println(ch);
+    }}
